a807afd5f4e7dfce4e54a8ec90bb5b709dcd1b2b
Adri <forczekadri@gmail.com>
2018-06-10 01:20:13 +0100

    removes main.erb, adds save() function for product

--- controller.rb
+++ controller.rb
@@ -8,39 +8,44 @@ get '/' do
   erb(:home)
 end
 
-get '/main' do
-  erb(:main)
-end
-
-get '/main/full_stock' do
+get '/full_stock' do
   @products = Product.all()
   erb(:full_stock)
 end
 
-get '/main/stock_levels' do
+get '/stock_levels' do
   @products = Product.all()
   erb(:stock_levels)
 end
 
-get '/main/categories' do
+get '/categories' do
   @categories = Category.all()
   erb(:categories)
 end
 
-get '/main/manufacturers' do
+get '/manufacturers' do
   @manufacturers = Manufacturer.all()
   erb(:manufacturers)
 end
 
-get '/main/manufacturers/:id' do
+get '/manufacturers/:id' do
   @manufacturer = Manufacturer.find(params[:id])
   erb(:manufacturer)
 end
 
 # # # #
 
-get '/main/new_product' do
+get '/new_product' do
   @manufacturers = Manufacturer.all()
   @categories = Category.all()
   erb(:new_product)
+end
+
+
+# # # #
+
+post '/full_stock' do
+  @product= Product.new(params)
+  @product.save()
+  redirect to '/full_stock'
 end
